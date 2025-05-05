a8f19bad04b81e7fc91e5ba1bce000a22a41c9bc
zhangyouhan <howardcyh@icloud.com>
2025-05-05 22:34:14 +0800

    init project

---
+++ src/main/java/com/howard/pairy/PairyApplication.java
@@ -0,0 +1,13 @@
+package com.howard.pairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PairyApplication.class, args);
+    }
+
+}
